b68cc78c9d9a969febaad8b0e34b4346b4e77ac2
devgeekay <gokula.kannan@neutrinos.co>
2020-12-24 13:27:52 +0530

    re-init

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # File-Manager
-PHP Based File Manager
--- footer.php
+++
@@ -1,4 +0,0 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.slim.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</body>
-</html>
